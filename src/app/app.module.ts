53649d7388aace5f025a17c63385db47d75ebcbc
aparjun <46344050+aparjun@users.noreply.github.com>
2023-10-28 18:31:29 +0530

    created all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoriesComponent } from './stories/stories.component';
+import { SprintsComponent } from './sprints/sprints.component';
+import { CreateStoryComponent } from './create-story/create-story.component';
+import { CreateSprintComponent } from './create-sprint/create-sprint.component';
+import { HomeComponent } from './home/home.component';
+import { TabViewModule } from 'primeng/tabview';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoriesComponent,
+    SprintsComponent,
+    CreateStoryComponent,
+    CreateSprintComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
