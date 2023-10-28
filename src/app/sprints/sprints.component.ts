772c05ae023d13a57e5ce3890f00a60f103d2a6a
aparjun <46344050+aparjun@users.noreply.github.com>
2023-10-29 01:02:07 +0530

    added story categorization logic

--- src/app/sprints/sprints.component.ts
+++ src/app/sprints/sprints.component.ts
@@ -9,7 +9,11 @@ export class SprintsComponent {
   @Output() onCreate = new EventEmitter<any>();
   sprintList: any = [];
   visible = false;
+  smallStoryLimit: number;
+  largeStoryLimit: number;
   constructor(){
+    this.smallStoryLimit = 0;
+    this.largeStoryLimit = 0;
   }
 
   openDialog(){
@@ -24,5 +28,16 @@ export class SprintsComponent {
     this.sprintList.push(event);
     this.onCreate.emit(this.sprintList.length);
     this.visible = false;
+    this.calculateSplittingPoints();
+  }
+
+  calculateSplittingPoints() {
+    const n = this.sprintList.length;
+    const pointsArray = this.sprintList.map((item: any) => item.points);
+    const lowerBound = Math.min(...pointsArray);
+    const upperBound = Math.max(...pointsArray);
+    const range = upperBound - lowerBound;
+    this.smallStoryLimit = lowerBound + (range * 1 / 3);
+    this.largeStoryLimit = lowerBound + (range * 2 / 3);
   }
 }
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -9,7 +9,11 @@ export class StoriesComponent {
   @Output() onCreate = new EventEmitter<any>();
   storyList: any = [];
   visible = false;
+  smallStoryLimit: number;
+  largeStoryLimit: number;
   constructor(){
+    this.smallStoryLimit = 0;
+    this.largeStoryLimit = 0;
   }
 
   openDialog(){
@@ -24,5 +28,16 @@ export class StoriesComponent {
     this.storyList.push(event);
     this.onCreate.emit(this.storyList.length);
     this.visible = false;
+    this.calculateSplittingPoints();
+  }
+
+  calculateSplittingPoints() {
+    const n = this.storyList.length;
+    const pointsArray = this.storyList.map((item: any) => item.points);
+    const lowerBound = Math.min(...pointsArray);
+    const upperBound = Math.max(...pointsArray);
+    const range = upperBound - lowerBound;
+    this.smallStoryLimit = lowerBound + (range * 1 / 3);
+    this.largeStoryLimit = lowerBound + (range * 2 / 3);
   }
 }
